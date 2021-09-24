14301846120adbc9e1f27a58e670f46af1d4de6e
giuspeppe9908 <66006287+giuspeppe9908@users.noreply.github.com>
2021-09-24 21:19:17 +0200

    Secondo Esercizio in Haskell

---
+++ Esercizio 2/SecondoScript.hs
@@ -0,0 +1,4 @@
+-- Scrivere un’espressione per rappresentare la condizione “1 minore o uguale di 0 oppure 0 minore o uguale di 1”, il cui valore deve essere True
+
+minore :: Bool
+minore = 1 <= 0 || 0 <= 1 
